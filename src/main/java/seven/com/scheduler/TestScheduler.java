6641721bde83e0cc0c5c0bafd6fac4d94314c387
forsev7en <254906593@qq.com>
2017-01-05 13:58:09 +0800

    添加数据源

--- src/main/java/seven/com/scheduler/TestScheduler.java
+++ src/main/java/seven/com/scheduler/TestScheduler.java
@@ -1,5 +1,7 @@
 package seven.com.scheduler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestScheduler {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestScheduler.class);
+
+    @Scheduled(cron = "0/10 * * * * ?")
+    public void executeFileDownLoadTask() {
+
+        // 间隔2分钟,执行工单上传任务
+        Thread current = Thread.currentThread();
+        System.out.println("定时任务1:"+current.getId());
+        logger.info("ScheduledTest.executeFileDownLoadTask 定时任务1:"+current.getId()+ ",name:"+current.getName());
+    }
+
     @Scheduled(cron = "0/20 * * * * ?")
-    public void  testprintln(){
-        System.out.println("-----------打印测试数据--打打打打打打 TestScheduler-----testprintln");
+    public void executeUploadTask() {
+
+        // 间隔1分钟,执行工单上传任务
+        Thread current = Thread.currentThread();
+        System.out.println("定时任务2:"+current.getId());
+        logger.info("ScheduledTest.executeUploadTask 定时任务2:"+current.getId() + ",name:"+current.getName());
+    }
+
+    @Scheduled(cron = "0/50 * * * * ?")
+    public void executeUploadBackTask() {
+
+        // 间隔3分钟,执行工单上传任务
+        Thread current = Thread.currentThread();
+        System.out.println("定时任务3:"+current.getId());
+        logger.info("ScheduledTest.executeUploadBackTask 定时任务3:"+current.getId()+ ",name:"+current.getName());
     }
 
 }
