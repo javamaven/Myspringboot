ab5781a89f7282f1e85de20600ddc354ba8dce24
javamaven <254906593@qq.com>
2017-01-20 02:41:38 +0800

    自带tomcat部署

--- src/main/java/seven/com/DemoApplication.java
+++ src/main/java/seven/com/DemoApplication.java
@@ -6,10 +6,12 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -30,7 +32,13 @@ import java.util.List;
 @EnableScheduling
 @ServletComponentScan
 @EnableConfigurationProperties({Myseven.class})
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
 
     /**
      * * fastJson 除了添加依赖之外还可以有2种方法
---
+++ src/main/resources/readme.txt
@@ -0,0 +1,3 @@
+多环境使用Logger
+
+http://412887952-qq-com.iteye.com/blog/2307244
